ad3c08c9a020f6e39b3fd4f0697e6f0f24a52318
Cody McNamara <codyjamesmcnamara@gmail.com>
2016-03-13 20:08:27 -0700

    set up rails backend with Todo model and json API

---
+++ app/controllers/api/todos_controller.rb
@@ -0,0 +1,39 @@
+class Api::TodosController < ApplicationController
+  # added this for degbugging with postman
+  # skip_before_filter :verify_authenticity_token
+
+  def index
+    @todos = Todo.all
+    render json: @todos
+  end
+
+  def show
+    @todo = Todo.find(params[:id])
+    render json: @todo
+  end
+
+  def create
+    @todo = Todo.create!(todo_params)
+    render json: @todo
+  end
+
+  def destroy
+    @todo = Todo.find(params[:id])
+    @todo.destroy!
+    # redirect_to todos_url
+    render json: "[{msg: it has been destroyed, mwahahahaha}]"
+  end
+
+  def update
+    @todo = Todo.find(params[:id])
+    @todo.update!(todo_params)
+    render json: @todo
+  end
+
+
+  private
+
+  def todo_params
+    params.require(:todos).permit(:title, :body, :done)
+  end
+end
